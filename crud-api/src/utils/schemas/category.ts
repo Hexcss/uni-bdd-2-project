c4bf71a35e5cf924840902888f25ca9f2a4df685
luciaZama <lzamanillol@gmail.com>
2024-03-23 22:57:49 +0100

    feat: Added categorySchema

---
+++ crud-api/src/utils/schemas/category.ts
@@ -0,0 +1,9 @@
+import Joi from "joi";
+
+const categorySchema = Joi.object({
+    id: Joi.string().alphanum().trim().min(8).required(),
+    name: Joi.string().min(3).required(),
+    description: Joi.string().allow('', null)
+})
+
+export default categorySchema;
