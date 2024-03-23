ee6f9392258f37e3be5a837f7b036db3af7deda3
luciaZama <lzamanillol@gmail.com>
2024-03-23 21:02:33 +0100

    feat: Finished categories.controller

--- crud-api/src/controllers/categories.controller.ts
+++ crud-api/src/controllers/categories.controller.ts
@@ -10,3 +10,64 @@ const getAllCategories = async (req: Request, res: Response): Promise<void> => {
     res.status(500).send({message: error.message});
   }
 }
+
+
+const createCategory = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const category = new Category(req.body);
+    const savedCategory = await category.save();
+    res.status(201).json(savedCategory);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+}
+
+const getCategoryById = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const category = await Category.findById(req.params.id);
+    if (!category) {
+      return res.status(404).send({ message: 'Categoría no encontrada.' });
+    }
+    res.json(category);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+}
+
+const updateCategory = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const updatedCategory = await Category.findOneAndUpdate({ id }, req.body, {
+      new: true,
+    });
+    if (!updatedCategory) {
+      return res.status(404).send({ message: 'Categoría no encontrada.' });
+    }
+    res.json(updatedCategory);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+}
+
+const deleteCategory = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const deletedCategory = await Category.findOneAndDelete({ id });
+    if (!deletedCategory) {
+      return res.status(404).send({ message: 'Categoría no encontrada.' });
+    }
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+}
+
+const categoriesController = {
+  getAllCategories,
+  createCategory,
+  getCategoryById,
+  updateCategory,
+  deleteCategory,
+}
+
+export default categoriesController;
