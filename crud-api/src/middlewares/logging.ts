955b17c6376d1f5959226b451eae7ebbe245d6ee
luciaZama <lzamanillol@gmail.com>
2024-03-23 21:48:52 +0100

    feat: Finished logging from middlewares

---
+++ crud-api/src/middlewares/logging.ts
@@ -0,0 +1,44 @@
+
+import morgan from 'morgan';
+import winston, {Logger, transports} from 'winston';
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
+        winston.format.printf(
+            (info: winston.Logform.TransformableInfo) => `${info.timestamp} [${info.level}]: ${info.message} ${info.httpMethod ? info.httpMethod : ''} ${info.url ? info.url : ''} `,
+
+        ),
+    ),
+    transports: [
+        new winston.transports.File({filename: 'combined.log', level: 'info'}),
+        new winston.transports.File({filename: 'errors.log', level: 'error'}),
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize({ all: true }),
+                winston.format.printf(
+                  (info: winston.Logform.TransformableInfo) =>
+                    `${info.timestamp} [${info.level}]: ${info.message} ${info.httpMethod ? info.httpMethod : ''} ${info.url ? info.url : ''} `,
+                ),
+            ),
+        }),
+    ],
+})
+
+const morganMiddleware = morgan(
+    function (tokens: any, req: any, res: any) {
+      return [
+        tokens.date(req, res, 'iso'),
+        `[${tokens.method(req, res)}]:`,
+        tokens.url(req, res),
+        `Status: ${tokens.status(req, res)}`,
+        `${tokens['response-time'](req, res)}ms`,
+      ].join(' ');
+    },
+    { stream: { write: (message: string) => logger.info(message.trim()) } },
+)
+
+const logginMiddleware = { logger, morganMiddleware };
+
+export default logginMiddleware;
