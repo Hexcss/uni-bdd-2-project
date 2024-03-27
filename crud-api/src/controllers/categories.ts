dc4ee4f50ce53c518490fc0c8742c7d5e1fb4068
luciaZama <lzamanillol@gmail.com>
2024-03-27 22:23:34 +0100

    feat: implemented crudService into Category controller

--- crud-api/src/controllers/categories.ts
+++ crud-api/src/controllers/categories.ts
@@ -1,65 +1,29 @@
-import Category from '../models/category'
+
 import {Request, Response} from 'express';
+import CrudServices from '../services/crudService';
+
 
 const getAllCategories = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const categories = await Category.find();
-    res.json(categories);
-
-  } catch (error) {
-    res.status(500).send({message: error.message});
-  }
+  CrudServices.getAll(req, res, "Category");
 }
 
 
 const createCategory = async (req: Request, res: Response) => {
-  try {
-    const category = new Category(req.body);
-    const savedCategory = await category.save();
-    res.status(201).json(savedCategory);
-  } catch (error) {
-    res.status(400).send({ message: error.message });
-  }
+  CrudServices.create(req, res, "Category");
 }
 
+
+
 const getCategoryById = async (req: Request, res: Response) => {
-  try {
-    const category = await Category.findById(req.params.id);
-    if (!category) {
-      return res.status(404).send({ message: 'Categoría no encontrada.' });
-    }
-    res.json(category);
-  } catch (error) {
-    res.status(500).send({ message: error.message });
-  }
+  CrudServices.getById(req, res, "Category");
 }
 
 const updateCategory = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const updatedCategory = await Category.findOneAndUpdate({ id }, req.body, {
-      new: true,
-    });
-    if (!updatedCategory) {
-      return res.status(404).send({ message: 'Categoría no encontrada.' });
-    }
-    res.json(updatedCategory);
-  } catch (error) {
-    res.status(500).send({ message: error.message });
-  }
+  CrudServices.update(req, res, "Category");
 }
 
 const deleteCategory = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const deletedCategory = await Category.findOneAndDelete({ id });
-    if (!deletedCategory) {
-      return res.status(404).send({ message: 'Categoría no encontrada.' });
-    }
-    res.status(204).send();
-  } catch (error) {
-    res.status(500).send({ message: error.message });
-  }
+ CrudServices.deleteById(req, res, "Category");
 }
 
 const categoriesController = {
