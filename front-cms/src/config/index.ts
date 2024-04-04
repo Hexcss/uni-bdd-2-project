59e25465d178db0febaf92197cd892698fecc73f
javier.cader <javier.cader@ct.uneatlantico.es>
2024-04-04 10:55:12 +0200

    feat: Front Name Changes

---
+++ front-cms/src/config/index.ts
@@ -0,0 +1,16 @@
+import { ensureVariableIsSet } from '../utils/functions';
+import { Environment } from '../utils/interfaces';
+
+export const environment: Environment = {
+  AUTH_API_URL: import.meta.env.VITE_AUTH_API_URL,
+  CRUD_API_URL: import.meta.env.VITE_CRUD_API_URL,
+  SECRET_KEY: import.meta.env.VITE_SECRET_KEY,
+};
+
+function ensureAllVariablesAreSet() {
+  Object.entries(environment).forEach(([key, value]) => {
+    ensureVariableIsSet(value, key);
+  });
+}
+
+ensureAllVariablesAreSet();
