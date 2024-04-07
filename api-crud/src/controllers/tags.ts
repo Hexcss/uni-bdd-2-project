63b2371d33ea1296f5ee32a0470383a66b9f7a67
luciaZama <lzamanillol@gmail.com>
2024-04-07 20:15:29 +0200

    fix: Redid getAllTags method from tags' controller

--- api-crud/src/controllers/tags.ts
+++ api-crud/src/controllers/tags.ts
@@ -1,8 +1,27 @@
 import { Request, Response } from 'express';
-import CrudServices from '../services/crudService';
+import { MongoService } from '../services';
+import { TagModel } from '../models';
+
+const tagService = new MongoService(TagModel);
 
 const getAllTags = async (req: Request, res: Response): Promise<void> => {
-  CrudServices.getAll(req, res, 'Tag');
+  const page = parseInt(req.query.page as string) || 1;
+  const limit = parseInt(req.query.limit as string) || 10;
+  const search = req.query.search as string; // Extract search term from query parameters
+
+  try {
+    const { data, totalCount } = await tagService.find(
+      {},
+      page,
+      limit,
+      null,
+      null,
+      search
+    );
+    res.json({ data, totalCount });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
 const createTag = async (req: Request, res: Response): Promise<void> => {
